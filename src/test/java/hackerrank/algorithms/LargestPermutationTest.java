8aaa5423cc7b954fb682b7a043048bc727034bb9
kromke <kromkemail@yandex.ru>
2020-09-25 17:10:50 +0300

    LargesPermutationTest-Falls

--- src/test/java/hackerrank/algorithms/LargestPermutationTest.java
+++ src/test/java/hackerrank/algorithms/LargestPermutationTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class LargestPermutationTest {
 
     @Test
-    void largestPermutationCase1() {
+    void largestPermutation_1swap() {
         int[] sourceArray = new int[] {1, 2, 3, 4};
         int numberOfSwaps = 1;
         int[] expectedArray = new int[] {4, 2, 3, 1};
@@ -16,19 +16,19 @@ class LargestPermutationTest {
     }
 
     @Test
-    void largestPermutationCase2() {
-        int[] sourceArray = new int[] {4, 2, 2, 2};
-        int numberOfSwaps = 1;
-        int[] expectedArray = new int[] {4, 2, 2, 2};
+    void largestPermutationCase_2swap() {
+        int[] sourceArray = new int[] {4, 2, 3, 5, 1};
+        int numberOfSwaps = 2;
+        int[] expectedArray = new int[] {5, 4, 3, 2, 1};
         int[] actualArray = LargestPermutation.largestPermutation(numberOfSwaps, sourceArray);
         assertArrayEquals(expectedArray, actualArray);
     }
 
     @Test
-    void largestPermutationCase3() {
-        int[] sourceArray = new int[] {1, 2, 3, 4};
-        int numberOfSwaps = 2;
-        int[] expectedArray = new int[] {4, 3, 2, 1};
+    void largestPermutationCase3(){
+        int[] sourceArray = new int[] {2, 1, 3};
+        int numberOfSwaps = 1;
+        int[] expectedArray = new int[] {3, 1, 2};
         int[] actualArray = LargestPermutation.largestPermutation(numberOfSwaps, sourceArray);
         assertArrayEquals(expectedArray, actualArray);
     }
